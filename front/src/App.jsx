a4ceb70383e8b8c2795a2fcddb3268a26dd3698a
farahkharbech <farah.kh@hotmail.fr>
2023-12-19 22:28:27 +0100

    farah second push

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
+    <div className="" > 
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/aboutUs' element={<AboutUs/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/admin' element={<Admin/>}/>
+ 
+
+    </Routes>
     </div>
+    </Router>
   );
 }
 
