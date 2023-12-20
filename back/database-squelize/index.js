34d6eb265401cf570b8c1bf7e1a8e6c0753f3ddc
houssemboulaares <houssem.boulaares.123@gmail.com>
2023-12-20 14:46:56 +0100

    solved

--- back/database-squelize/index.js
+++ back/database-squelize/index.js
@@ -4,7 +4,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(
   "team2",
   "root",
-  "farahkh",
+  "mysqL111",
   {
     host: "localhost",
     dialect: "mysql",
