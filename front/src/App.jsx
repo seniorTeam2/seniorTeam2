f60e622546201cd7dfb5eabe3c34db8c7ed94239
fares1902 <fares.chaouali99@gmail.com>
2023-12-21 10:24:56 +0100

    doneFares

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,13 +8,17 @@ import Contact from './Components/Contact.jsx';
 import FlashSales from './Components/FlashSales.jsx';
 import BrowseCategory from './Components/BrowseCategory.jsx';
 import BestSellingProducts from './Components/BestSellingProducts.jsx';
-
+import Signup from './Components/Signup.jsx';
+import Login from './Components/Login.jsx';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/cart'element={<Cart/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+
         <Route path='/AboutUs' element={<AboutUs/>}></Route>
         <Route path='/admin' element={<Admin/>}></Route>
         <Route path='/AboutUs' element={<AboutUs/>}></Route>
