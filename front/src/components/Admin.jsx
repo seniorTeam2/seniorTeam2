9615401d5d08f8a0569db593f85f2e3441fcf2fd
farahkharbech <farah.kh@hotmail.fr>
2023-12-21 11:05:32 +0100

    farah charts hardcoded

--- front/src/components/Admin.jsx
+++ front/src/components/Admin.jsx
@@ -20,13 +20,13 @@ function Admin() {
             className='bg-gray-200 p-2 text-xs rounded w-56 h-9 right-36'/>
               <IoSearchOutline size={25} className='absolute right-10 top-7'/>
         </div>
-   
         
-     
-      
         </div>
        </nav>
-
+       <div className='flex justify-center rounded mt-52 gap-x-72 '>
+       <div><img src="https://canvasjs.com/wp-content/uploads/images/gallery/javascript-charts/overview/javascript-charts-graphs-index-data-label.png"/></div>
+      <div><img src="https://chartio.com/assets/cad7f1/tutorials/charts/pie-charts/eaeaa38b48b5dc5229d876831ab031f38c2bedb2140cfab55b8f1ff22842cce9/pie-chart-example-1.png"/></div>
+      </div>
     </div>
   )
 }
--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import FlashSales from './FlashSales';
 
-import { NavLink } from 'react-router-dom';
-
 import { FaArrowRight } from "react-icons/fa";
-import { FaRegHeart } from "react-icons/fa";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { IoSearchOutline } from "react-icons/io5";
+
 import BrowseCategory from './BrowseCategory';
 import BestSellingProducts from './BestSellingProducts';
 import Details from './Details';
@@ -23,25 +19,7 @@ const Home = () => {
                 <option >English</option>
             </select>
         </div>
-        <nav >
-        
-            <div className='flex justify-center gap-8 mt-11 mb-6'>
-            <h1 className='absolute left-20 font-bold text-xl'>Exlusive</h1> 
-            <NavLink to={'/home'} >Home</NavLink>
-            <NavLink to={'/contact'}>Contact</NavLink>
-            <NavLink to={'/AboutUs'}>AboutUs</NavLink>
-            <NavLink to={'/sing-up'}>Sing up</NavLink>
-            <div className='w-auto h-8 flex float-right gap-16 absolute right-10 top-20'>
-                <input type="search"
-                placeholder='What are you looking for?'
-                className='bg-gray-200 p-2 text-xs rounded w-56 h-9'/>
-               <IoSearchOutline size={25} className='absolute right-48 top-1'/>
-                <FaRegHeart size={25}/>
-                <AiOutlineShoppingCart  size={25}/>
-
-            </div>
-            </div>
-           </nav>
+       
 
             <Navbar/>
 
