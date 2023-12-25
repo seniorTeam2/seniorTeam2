eb313970851fab452d29d0e8b03c30685f8ea2ac
salimBenSlim1 <salimbenslim09@gmail.com>
2023-12-25 11:36:04 +0100

    done

--- back/index.js
+++ back/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use('/auth', authRoutes);
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
-app.use('/api/products',CartRouter)
+app.use('/api/cart',CartRouter)
  app.use('/api/products',prodRouter)
  app.use('/api/users',userRouter)
  app.use('/api/categories',categRouter)
