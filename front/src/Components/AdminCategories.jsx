a63816293f6a734f063f526d11aa05c8360a5222
farahkharbech <farah.kh@hotmail.fr>
2023-12-25 15:41:47 +0100

    crud admin done

--- front/src/Components/AdminCategories.jsx
+++ front/src/Components/AdminCategories.jsx
@@ -8,7 +8,14 @@ function AdminCat() {
 
     const [categs,setCategs] = useState([])
     const [refresh,setRefresh] = useState(false)
+    const [show,setShow] = useState(0)
+    const [name,setName] = useState("")
+
     let location = useLocation();
+
+  console.log("name" , name)
+
+
 
     useEffect(()=>{
         axios.get('http://localhost:3000/api/categories/allCategories').then((res)=>{setCategs(res.data)})
@@ -20,6 +27,10 @@ function AdminCat() {
         .catch((err)=>{console.log(err)})
     }
 
+    const updateCateg = (id,newdata) => {
+      axios.put(`http://localhost:3000/api/categories/updateCategory/${id}`,newdata).then((res)=>{setRefresh(!refresh)})
+      .catch((err)=>{console.log(err)})
+  }
 
   return (
     <div>
@@ -40,7 +51,26 @@ function AdminCat() {
         <div key={i} className='flex-wrap grid-cols-5 mb-6   hover:box-content '>
         <img className=' h-40 w-30 shadow-lg rounded-3xl' src={el.CategoryImage} alt="" />
         <h1 className='text-'>{el.NameCategory}</h1>
-        <button className='bg-red text-white rounded w-20 h-9 my-5' onClick={()=>{deleteCateg(el.CategoryID)}}> delete </button>
+        <button className='bg-white hover:bg-red text-black rounded w-20 h-9 my-5' onClick={()=>{deleteCateg(el.CategoryID)}}> delete </button>
+        <button className='bg-white hover:bg-red text-black rounded w-20 h-9 my-5'  onClick={()=>{setShow(el.CategoryID)}}> Update </button>
+      
+      
+        <div>{show===el.CategoryID && 
+        <div className='border'>
+           <div className="pt-0 mb-3 my-10">
+     <input
+          type="text"
+          placeholder="Category Name"
+          className="relative w-full px-3 py-3 text-sm text-red-600 placeholder-gray-600 bg-gray border-0 rounded shadow"
+          onChange={(e)=>{setName(e.target.value)}}
+        />
+    </div>
+
+   <button className='hover:shadow-lg hover:bg-white px-6 py-3 mb-1 mr-1 text-sm font-bold text-black bg-red uppercase rounded shadow' onClick={()=>{updateCateg(el.CategoryID,{NameCategory : name});
+   setShow(0)}}> Validate </button>
+        </div>
+        
+        }</div>
         </div>
     ))}
        </div>
